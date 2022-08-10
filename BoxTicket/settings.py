51130cba25d778791d503005dae2a83abe65f26f
samsul hadi <samsulhadikediri@gmail.com>
2022-08-10 20:55:21 +0700

    Fix: email username

--- BoxTicket/settings.py
+++ BoxTicket/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
