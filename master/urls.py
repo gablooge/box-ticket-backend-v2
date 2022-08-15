e943a9c63d69ea298eb7343a241404e15a0bf662
Samsul Hadi <samsulhadikediri@gmail.com>
2022-08-15 09:48:45 +0700

    12 feat implement get regions (#14)
    * Feat: Implement Get Countries
    
    * fix: change field name to country_name
    
    * chore: remove comment
    
    * feat: implement get regions
    
    * chore: change ApiView to ReadOnlyModelViewSet

--- master/urls.py
+++ master/urls.py
@@ -1,8 +1,9 @@
-from master.views import CountryView
+from master.views import CountryView, RegionView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 
 router.register("GetCountries", CountryView)
+router.register("GetRegions", RegionView)
 
 urlpatterns = router.urls
--- master/views.py
+++ master/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from BoxTicket.response import customResponse
-from master.models import Country
-from master.serializers import CountrySerializers
+from master.models import Country, Region
+from master.serializers import CountrySerializers, RegionSerializers
 
 
 class CountryView(ReadOnlyModelViewSet):
@@ -22,6 +22,43 @@ class CountryView(ReadOnlyModelViewSet):
             method_status = False
             return customResponse(
                 {"CountriesList": serializer.data},
+                end_error_message,
+                error_message,
+                error_code,
+                method_status,
+            )
+        return customResponse({"RegionsList": serializer.data})
+
+
+class RegionView(ReadOnlyModelViewSet):
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializers
+
+    def get_queryset(self):
+        country_id = self.request.GET.get("country_id", None)
+        queryset = self.queryset
+        if country_id:
+            try:
+                country_id = int(country_id)
+                queryset = queryset.filter(country__id=country_id)
+            except Exception:
+                queryset = {}
+        return queryset
+
+    def list(self, request):
+        page = self.paginate_queryset(self.get_queryset())
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+        if not serializer.data:
+            end_error_message = "No Data Found"
+            error_message = "No Data Found"
+            error_code = 1
+            method_status = False
+            return customResponse(
+                {"RegionsList": serializer.data},
                 end_error_message,
                 error_message,
                 error_code,
